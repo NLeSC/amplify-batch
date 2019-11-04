100f2abe286d7e8c44e495dfddbd1eea500d97f4
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2019-11-04 10:01:49 +0100

    Wrap app in authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { Authenticator } from 'aws-amplify-react';
+import { UsernameAttributes } from 'aws-amplify-react/lib-esm/Auth/common/types';
+
+Amplify.configure(awsconfig);
+Storage.configure({ level: 'private' });
+
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator usernameAttributes={UsernameAttributes.EMAIL}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Authenticator>
   );
 }
-
-export default App;
